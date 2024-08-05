87052fe64945ad87a240cd21f761d782d7e28558
hady-awayda <hady.awayda@gmai.com>
2024-08-06 02:42:11 +0300

    Fixed import bug in auth controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import UserController from "../controllers/user.controller.js";
-import AuthController from "../controllers/auth.controller.js";
+// import AuthController from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 // Auth Routes
-router.post("/users/signup", AuthController.signup);
+// router.post("/users/signup", AuthController.signup);
 // router.post("/users/login", AuthController.login);
 // router.get("/users/logout", AuthController.logout);
 
