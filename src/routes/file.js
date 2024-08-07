c5ef8612f9303155fd9d1c0a28828fe70bb07394
hady-awayda <hady.awayda@gmai.com>
2024-08-07 12:02:31 +0300

    Switched location of file.js

--- src/routes/file.js
+++ src/routes/file.js
@@ -1,8 +1,24 @@
 import express from "express";
+import userAuth from "../middleware/userAuthorization.js";
+import adminAuth from "../middleware/adminAuthorization.js";
 import FileUploadController from "../controllers/fileUploadController.js";
+import {
+  setCreatedBy,
+  setUpdatedBy,
+  setDeletedBy,
+} from "../middleware/trackChanges.js";
 
 const router = express.Router();
 
-router.get("/", FileUploadController.getFiles);
+router.get("/", userAuth, FileUploadController.getFiles);
+router.get("/:id", userAuth, FileUploadController.getFile);
+router.post(
+  "/upload",
+  adminAuth,
+  setCreatedBy,
+  FileUploadController.uploadFile
+);
+router.patch("/:id", adminAuth, setUpdatedBy, FileUploadController.updateFile);
+router.delete("/:id", adminAuth, setDeletedBy, FileUploadController.deleteFile);
 
 export default router;
