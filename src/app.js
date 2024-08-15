fba9a751e247d0a6046f8db0464de7bc91ae79a8
hady-awayda <hady.awayda@gmail.com>
2024-08-15 03:20:16 +0300

    Updated default port to 8000

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ import withdrawalRoutes from "./routes/withdrawal.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
